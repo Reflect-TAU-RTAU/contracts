d8fc87883dfdedfed7e70d2c58b906d17385d2d2
endogen <081081@gmx.net>
2022-03-29 03:09:15 +0200

    Finished treasury

--- con_reflecttau_v2_treasury.py
+++ con_reflecttau_v2_treasury.py
@@ -3,25 +3,33 @@ I = importlib
 import con_reflecttau_v2 as rtau
 import con_rocketswap_official_v1_1 as rswp
 
+@construct
+def init():
+	contract.set(ctx.this)
+
 @export
 def execute(payload: dict, caller: str):
 	assert ctx.caller == rtau.contract(), 'You are not allowed to do that'
 
-	"""
-	This is the key that each operator needs to submit into metadata. 
-	The value of that key needs to be 'agreed' or otherwise this will fail. 
-	"""
-	key = f"{ctx.this}:{payload['function']}:{payload['amount']}:{payload['to']}"
+	key = f"{contract.get()}:{payload['function']}:{payload['amount']}:{payload['to']}"
 	rtau.assert_operators_agree(agreement=key)
 
-	if payload['function'] == 'send_token':
-		return send_token(payload['contract'], payload['amount'], payload['to'])
+	if payload['function'] == 'withdraw_token':
+		return withdraw_token(payload['contract'], payload['amount'], payload['to'])
 
-	if payload['function'] == 'send_lp':
-		return send_lp(payload['contract'], payload['amount'], payload['to'])
+	if payload['function'] == 'withdraw_lp':
+		return withdraw_lp(payload['contract'], payload['amount'], payload['to'])
 
-def send_token(token_contract: str, amount: float, to: str):
+@export
+def deposit_token(token_contract: str, amount: float):
+	return I.import_module(token_contract).transfer_from(amount=amount, to=contract.get(), main_account=ctx.caller)
+
+@export
+def deposit_lp(token_contract: str, amount: float):
+	return rswp.transfer_liquidity_from(contract=token_contract, to=contract.get(), main_account=ctx.caller, amount=amount)
+
+def withdraw_token(token_contract: str, amount: float, to: str):
 	return I.import_module(token_contract).transfer(amount=amount, to=to)
 
-def send_lp(token_contract: str, amount: float, to: str):
+def withdraw_lp(token_contract: str, amount: float, to: str):
 	return rswp.transfer_liquidity(contract=token_contract, amount=amount, to=to)
